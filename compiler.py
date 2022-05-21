9281b0c56f86d42c23c3f11acbd7051102e4e1d3
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2022-05-21 03:53:49 +0200

    Add files via upload

--- compiler.py
+++ compiler.py
@@ -1,12 +1,16 @@
 import re
 
+from numpy import true_divide
+
 f = open("main.drum", "r")
 code = f.readlines()
 
 asm = []
-data = []
+data = ["\nfreg dd 0"]
+adopted_filename = ""
 functions = []
 function_stack = [None]
+append_without_parsing = False
 macros = [
     "%macro write_string 2 ",
         "mov   eax, 4",
@@ -32,9 +36,8 @@ def create_function_if_nonexistent(function_name):
     if not exists:
         functions.append([function_name, []])
 
-def asm_append(line, ignore_stack=False):
-    #print(line, function_stack[0])
-    func = function_stack[0]
+def asm_append(line, ignore_stack=False, stack_offset=0):
+    func = function_stack[0+stack_offset]
     if func == None or ignore_stack:
         asm.append(line)
     elif func != None:
@@ -65,7 +68,7 @@ def if_statement(line, index):
     }
     
     args = re.split('>|<|==|>=|<=|!=|\n', line[3:])
-    function_name = "if_" + str(index)
+    function_name = adopted_filename + "_" + "if_" + str(index)
     for i, x in enumerate(args):
         args[i] = args[i].replace('=', '').strip()
         args[i] = get_variable_name(args[i], enclose=True)
@@ -80,8 +83,13 @@ def if_statement(line, index):
     
     function_stack.insert(0, function_name)
     create_function_if_nonexistent(function_name)
-    asm_append(function_name + "_return:", ignore_stack=True)
+    asm_append(function_name + "_return:", stack_offset=1)
 
+def function_declaration(line):
+    function_name = "FUNC_" + line.split()[1]
+    function_stack.insert(0, function_name)
+    create_function_if_nonexistent(function_name)
+    
 def variable_operation(line):
     line = line.replace(" ", "")
     operation = None
@@ -103,7 +111,6 @@ def variable_operation(line):
         asm_append("sub " + a + ", eax")
     if operation == "=":
         asm_append("mov " + a + ", eax")
-    #print(a + operation + b)
     
 def end_statement():
     function_stack.pop(0)
@@ -115,6 +122,22 @@ def print_statement(line):
 def goto_statement(line):
     asm_append(line.replace("goto", "jmp").lstrip().rstrip())    
     
+def call_statement(line):
+    line = line.split()
+    function_name = line[1].strip()
+    if function_stack[0]:
+        asm_append("jmp"+ " FUNC_" + function_name) 
+    else:
+        asm_append(line[0] + " FUNC_" + function_name)   
+
+def adopt_declaration(line):
+    global adopted_filename
+    f = open(line.split()[1].strip(), "r")
+    code = f.readlines()
+    adopted_filename = line.split()[1].replace(".drum", "")
+    parse_lines(code)
+    adopted_filename = ""
+        
 def string_declaration(line):
     string_name = line.split()[1]
     string_value = line[line.index("=")+1:].lstrip().rstrip('\n')
@@ -130,28 +153,43 @@ def label_declaration(line):
     asm_append(line.split()[1] + ":")
 
 def parse_lines(lines):
+    global append_without_parsing
     for i, line in enumerate(lines):
         line_split = line.split()
         if(line_split):
             keyword = line_split[0]
         else: keyword = "N/A"
         
-        if keyword == "string":
-            string_declaration(line)         
-        if keyword == "numeral":
-            numeral_declaration(line)  
-        if keyword == "print":
-            print_statement(line)          
-        if keyword == "label":
-            label_declaration(line)
-        if keyword == "goto":
-            goto_statement(line)
-        if keyword == "if":
-            if_statement(line, i)
-        if keyword == "end":
-            end_statement()
-        if get_variable_name(keyword, allownone=True):
-            variable_operation(line)
+        if not append_without_parsing:
+            if keyword == "string":
+                string_declaration(line)         
+            if keyword == "numeral":
+                numeral_declaration(line)  
+            if keyword == "print":
+                print_statement(line)          
+            if keyword == "label":
+                label_declaration(line)
+            if keyword == "goto":
+                goto_statement(line)
+            if keyword == "if":
+                if_statement(line, i)
+            if keyword == "function":
+                function_declaration(line)
+            if keyword == "call":
+                call_statement(line)
+            if keyword == "end":
+                end_statement()
+            if keyword == "adopt":
+                adopt_declaration(line)
+            if keyword == "<asm":
+                append_without_parsing = True
+            if get_variable_name(keyword, allownone=True):
+                variable_operation(line)
+        else:
+            if line.strip() == "asm>":
+                append_without_parsing = False
+            else:
+                asm_append(line.strip())
 
 def compile():
     parse_lines(code)
@@ -162,10 +200,19 @@ def compile():
         
     #Add functions
     for x in functions:
-        result += "\n\n" + x[0] + ":"
-        for y in x[1]:
-            result += "\n" + y
-        result += "\njmp " + x[0] + "_return"
+        if x[0][0] != "%":
+            result += "\n\n" + x[0] + ":"
+            for y in x[1]:
+                result += "\n" + y
+            if("FUNC_" not in x[0]):
+                result += "\njmp " + x[0] + "_return"
+            else:
+                result += "\nret"
+        else:
+            result += "\n\n" + x[0]
+            for y in x[1]:
+                result += "\n" + y
+            result += "\n%endmacro"
         
     result += "\n\nglobal _start\n_start:"
     for x in asm:
@@ -179,3 +226,4 @@ def compile():
     return result
            
 print(compile())
+#compile()
